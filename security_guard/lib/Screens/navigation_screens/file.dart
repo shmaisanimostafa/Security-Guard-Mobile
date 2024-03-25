56ef9f28bff3fa5a95ba7ed459bd099ec460a0f7
Mostafa Shmaisani <shmaisanimostafa@gmail.com>
2024-03-25 18:44:48 +0200

    Develop the Upload File Card

--- security_guard/lib/Screens/navigation_screens/file.dart
+++ security_guard/lib/Screens/navigation_screens/file.dart
@@ -1,3 +1,4 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 class FileUploadScreen extends StatelessWidget {
@@ -11,8 +12,41 @@ class FileUploadScreen extends StatelessWidget {
       ),
       body: Center(
         child: TextButton(
-          child: const Text('Select File'),
-          onPressed: () {},
+          style: TextButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          onPressed: () {
+            // Add file upload functionality here
+          },
+          child: DottedBorder(
+            borderType: BorderType.RRect,
+            radius: const Radius.circular(20),
+            dashPattern: const [10, 10],
+            color: Colors.grey,
+            strokeWidth: 2,
+            child: Card(
+              margin: const EdgeInsets.all(20),
+              color: Colors.amber,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const SizedBox(
+                height: 200.0,
+                width: 200.0,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.upload_file, size: 50.0, color: Colors.black),
+                      Text("Upload File Here"),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
