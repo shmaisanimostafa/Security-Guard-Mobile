05651252e03b2949cb9d20873701b33d597498bc
Mostafa Shmaisani <shmaisanimostafa@gmail.com>
2024-03-26 10:12:42 +0200

    Edit the Upload Image Button, in Scan Screen

--- security_guard/lib/Screens/scan.dart
+++ security_guard/lib/Screens/scan.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -61,13 +62,43 @@ class _ScanState extends State<Scan> {
                   )
                 : const Text('No image selected'),
             const SizedBox(height: 20.0),
-            ElevatedButton(
+            TextButton(
+              style: TextButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
               onPressed: () {
-                // debugPrint('Scan');
-                // Navigator.pop(context);
                 pickImage(ImageSource.gallery);
               },
-              child: const Text('Upload Image'),
+              child: DottedBorder(
+                borderType: BorderType.RRect,
+                radius: const Radius.circular(20),
+                dashPattern: const [10, 10],
+                color: Colors.grey,
+                strokeWidth: 2,
+                child: Card(
+                  margin: const EdgeInsets.all(20),
+                  color: Colors.amber,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const SizedBox(
+                    height: 200.0,
+                    width: 200.0,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.image_search_rounded,
+                              size: 50.0, color: Colors.black),
+                          Text("Upload Image Here"),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
             const SizedBox(height: 20.0),
             ElevatedButton(
